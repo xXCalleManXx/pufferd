f9d7a5f37a43bcca0ca0e8d007c350b1246ec33f
puffrfish <puffrfish@users.noreply.github.com>
2016-03-12 23:47:57 -0500

    Add license headers

--- legacy/legacy.go
+++ legacy/legacy.go
@@ -1,7 +1,26 @@
+/*
+ Copyright 2016 Padduck, LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ 	http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+// Package legacy implements the legacy API for compatibility with PufferPanel v0.8.x
 package legacy
 
-import "github.com/gin-gonic/gin"
-import "github.com/PufferPanel/pufferd/environments/system"
+import (
+	"github.com/PufferPanel/pufferd/environments/system"
+	"github.com/gin-gonic/gin"
+)
 
 func GetServerInfo(c *gin.Context) {
 }
@@ -16,7 +35,7 @@ func DeleteServer(c *gin.Context) {
 }
 
 func ServerPower(c *gin.Context) {
-    system.StartServer(c)
+	system.StartServer(c)
 }
 
 func ServerConsole(c *gin.Context) {
