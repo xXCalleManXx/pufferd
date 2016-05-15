bc16be2562431cf211ed70e888ee19e684c7fa16
Lord_Ralex <lordralex@gmail.com>
2016-05-15 16:01:22 -0400

    Improve logging even more - Add levels as variables for usability - Hide messages if logging level is too low

--- logging/logger.go
+++ logging/logger.go
@@ -18,45 +18,74 @@ package logging
 
 import "fmt"
 
+type level struct {
+	scale byte;
+	display string;
+}
+
+var (
+	DEBUG level = level{scale: 7, display: "DEBUG"};
+	INFO level = level{scale: 31, display: "INFO"};
+	WARN level = level{scale: 63, display: "WARN"};
+	ERROR level = level{scale: 127, display: "ERROR"};
+	CRITICAL level = level{scale: 255, display: "CRITICAL"};
+	loggingLevel = INFO;
+)
+
 func Info(msg string, data ...interface{}) {
-	log("INFO", msg, data);
+	log(INFO, msg, data);
 }
 
 func Warn(msg string, data ...interface{}) {
-	log("WARN", msg, data);
+	log(WARN, msg, data);
 }
 
 func Debug(msg string, data ...interface{}) {
-	log("DEBUG", msg, data);
+	log(DEBUG, msg, data);
 }
 
 func Error(msg string, data ...interface{}) {
-	log("ERROR", msg, data);
+	log(ERROR, msg, data);
+}
+
+func Critical(msg string, data ...interface{}) {
+	log(CRITICAL, msg, data);
 }
 
 func Infof(msg string, data ...interface{}) {
-	logf("INFO", msg, data);
+	logf(INFO, msg, data);
 }
 
 func Warnf(msg string, data ...interface{}) {
-	logf("WARN", msg, data);
+	logf(WARN, msg, data);
 }
 
 func Debugf(msg string, data ...interface{}) {
-	logf("DEBUG", msg, data);
+	logf(DEBUG, msg, data);
 }
 
 func Errorf(msg string, data ...interface{}) {
-	logf("ERROR", msg, data);
+	logf(ERROR, msg, data);
 }
 
-func log(level string, msg string, data ...interface{}) {
+func Criticalf(msg string, data ...interface{}) {
+	logf(CRITICAL, msg, data);
+}
+
+func SetLevel(lvl level) {
+	loggingLevel = lvl;
+}
+
+func log(lvl level, msg string, data ...interface{}) {
+	if(lvl.scale < loggingLevel.scale) {
+		return;
+	}
 	var dataLength = len(data[0].([]interface{}));
 	if(data == nil || dataLength == 0) {
-		fmt.Printf("[%s] %s\n", level, msg);
+		fmt.Printf("[%s] %s\n", lvl.display, msg);
 	} else {
 		cast := make([]interface{}, 3);
-		cast[0] = level;
+		cast[0] = lvl.display;
 		cast[1] = msg;
 		if(dataLength == 1) {
 			cast[2] = data[0].([]interface{})[0];
@@ -67,10 +96,13 @@ func log(level string, msg string, data ...interface{}) {
 	}
 }
 
-func logf(level string, msg string, data ...interface{}) {
+func logf(lvl level, msg string, data ...interface{}) {
+	if(lvl.scale < loggingLevel.scale) {
+		return;
+	}
 	if(data == nil || len(data[0].([]interface{})) == 0) {
-		fmt.Printf("[%s] %s\n", level, msg);
+		fmt.Printf("[%s] %s\n", lvl.display, msg);
 	} else {
-		fmt.Printf("[%s] %s\n", level, fmt.Sprintf(msg, data[0].([]interface{})...));
+		fmt.Printf("[%s] %s\n", lvl.display, fmt.Sprintf(msg, data[0].([]interface{})...));
 	}
 }
