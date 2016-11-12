91c2c7d8d2c77c5776e295705e32a821b0e93cba
Joshua Taylor <lordralex@gmail.com>
2016-11-12 10:38:54 -0600

    Add migration from Scales to pufferd (#18)

---
+++ migration/migration.go
@@ -0,0 +1,98 @@
+package migration
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+
+	"github.com/pufferpanel/pufferd/logging"
+	"encoding/json"
+	"github.com/pufferpanel/pufferd/utils"
+	"os"
+	"github.com/pufferpanel/pufferd/programs"
+	"github.com/pufferpanel/pufferd/data/templates"
+)
+
+const Scales = "/srv/scales/data"
+
+func MigrateFromScales() {
+	templates.CopyTemplates()
+	os.MkdirAll(programs.ServerFolder, 0755)
+
+	programFiles, err := ioutil.ReadDir(Scales)
+	if err != nil {
+		logging.Critical("Error reading from old Scales folder", err)
+		return
+	}
+	for _, element := range programFiles {
+		if element.IsDir() {
+			continue
+		}
+		id := strings.TrimSuffix(element.Name(), filepath.Ext(element.Name()))
+		logging.Infof("Attempting to migrate %s", id)
+		data, err := ioutil.ReadFile(utils.JoinPath(Scales, element.Name()))
+		if err != nil {
+			logging.Error("Error read server config", err)
+			continue
+		}
+		scales := scalesServer{}
+		err = json.Unmarshal(data, &scales)
+		if err != nil {
+			logging.Error("Error read server config", err)
+			continue
+		}
+		newPath := utils.JoinPath("data", "servers", scales.Name)
+		err = os.Rename(utils.JoinPath("/home", scales.User), newPath)
+		if err != nil {
+			logging.Error("Error moving folder", err);
+			continue
+		}
+
+		err = filepath.Walk(newPath, func(name string, info os.FileInfo, err error) {
+			if err == nil {
+				err = os.Chown(name, os.Getuid(), os.Getgid())
+			}
+			return err
+		})
+		if err != nil {
+			logging.Error("Error changing owner of folder", err);
+			continue
+		}
+		serverData := make(map[string]interface{})
+		serverData["ip"] = scales.Gamehost
+		serverData["port"] = scales.Gameport
+		if scales.Plugin == "minecraft" {
+			serverData["memory"] = scales.Build.Memory
+		} else if scales.Plugin == "srcds" {
+			serverData["appid"] = scales.Startup.Variables.Build_Params
+			serverData["gametype"] = scales.Startup.Variables.Game
+			serverData["map"] = scales.Startup.Variables.Map
+		}
+		programs.Create(scales.Name, scales.Plugin, serverData)
+	}
+}
+
+type scalesServer struct {
+	Name     string            `json:"name,omitempty"`
+	User     string            `json:"user,omitempty"`
+	Build    scalesServerBuild `json:"build,omitempty"`
+	Gameport int               `json:"gameport,omitempty"`
+	Gamehost string            `json:"gamehost,omitempty"`
+	Plugin   string            `json:"plugin,omitempty"`
+	Startup  scalesServerStartup `json:"startup,omitempty"`
+}
+
+type scalesServerBuild struct {
+	Memory int `json:"memory,omitempty"`
+}
+
+type scalesServerStartup struct {
+	Variables scalesServerStartupVariables `json:"variables,omitempty"`
+}
+
+type scalesServerStartupVariables struct {
+	Build_Params string `json:"build_params,omitempty"`
+	Game         string `json:"game,omitempty"`
+	Map          string `json:"map,omitempty"`
+	Players      string `json:"players,omitempty"`
+}
