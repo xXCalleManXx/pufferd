8b75e017cad81cd40f5743d300cc5984e5e380bf
Lord_Ralex <lordralex@gmail.com>
2017-05-14 19:39:30 -0500

    Fix template routing

--- routing/template/template.go
+++ routing/template/template.go
@@ -13,6 +13,7 @@ func RegisterRoutes(e *gin.Engine) {
 	{
 		l.GET("", httphandlers.OAuth2Handler("node.templates", false), GetTemplates)
 		l.GET("/:id", httphandlers.OAuth2Handler("node.templates", false), GetTemplate)
+		l.POST("/:id", httphandlers.OAuth2Handler("node.templates.edit", false), EditTemplate)
 	}
 }
 
@@ -21,7 +22,25 @@ func GetTemplates(c *gin.Context) {
 }
 
 func GetTemplate(c *gin.Context) {
-	name, exists := c.GetQuery("name")
+	name, exists := c.GetQuery("id")
+	if !exists || name == "" {
+		http.Respond(c).Fail().Code(400).MessageCode(http.INVALIDREQUEST).Message("no template name provided").Send()
+		return
+	}
+	data, err := programs.GetPlugin(name)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.Respond(c).Fail().Code(404).MessageCode(http.NOFILE).Message("no template with provided name").Send()
+		} else {
+			http.Respond(c).Fail().Code(500).MessageCode(http.UNKNOWN).Message("error reading template").Send()
+		}
+	} else {
+		http.Respond(c).Code(200).Data(data).Send()
+	}
+}
+
+func EditTemplate(c *gin.Context) {
+	name, exists := c.GetQuery("id")
 	if !exists || name == "" {
 		http.Respond(c).Fail().Code(400).MessageCode(http.INVALIDREQUEST).Message("no template name provided").Send()
 		return
