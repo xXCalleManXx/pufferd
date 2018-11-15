a4c55fc06c16ce9790a803f41383c65b8ee13f11
Lord_Ralex <lordralex@gmail.com>
2018-11-14 20:35:06 -0600

    Fix command executor to replace tokens

--- programs/operations/ops/impl/command/command.go
+++ programs/operations/ops/impl/command/command.go
@@ -51,9 +51,12 @@ type CommandOperationFactory struct {
 }
 
 func (of CommandOperationFactory) Create(op ops.CreateOperation) ops.Operation {
-	commands := common.ToStringArray(op.OperationArgs["commands"])
+	cmds := common.ToStringArray(op.OperationArgs["commands"])
+	for k, v := range cmds {
+		cmds[k] = common.ReplaceTokens(v, op.DataMap)
+	}
 	env := common.ReplaceTokensInMap(op.EnvironmentVariables, op.DataMap)
-	return Command{Commands: commands, Env: env}
+	return Command{Commands: cmds, Env: env}
 }
 
 func (of CommandOperationFactory) Key() string {
